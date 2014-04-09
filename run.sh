8222b11da5614ca822298b7beb1956ef9ce61795
Ethan Kruse <eakruse@uw.edu>
2014-04-09 16:55:11 -0700

    Removed the data files by default.

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ echo 'run.sh : (1/4) Downloaded data.'
 python ./transit_cut.py
 echo 'run.sh : (2/4) Done loading files.'
 python ./mcmc_run.py &
-echo 'run.sh : (3/4) Plotted results from the best fit model.'
+echo 'run.sh : (3/4) Plotting results from the best fit model in the background.'
 python ./mcmc_analyze.py &
-echo 'run.sh : (4/4) Done with the analysis.'
+echo 'run.sh : (4/4) Running the analysis in the background.'
